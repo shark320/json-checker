44ccb3e8e203ad05ba860f3f460b8eb29b81bd96
Volodymyr Pavlov <vpavlov@gk-software.com>
2025-01-15 17:08:03 +0100

    build upd

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     kotlin("jvm") version "2.0.20"
+    application
 }
 
 group = "com.vpavlov"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
@@ -19,4 +20,16 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(21)
+}
+
+application {
+    mainClass.set("com.vpavlov.MainKt")
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = application.mainClass.get()
+    }
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
 }
--- src/main/kotlin/com/vpavlov/Main.kt
+++ src/main/kotlin/com/vpavlov/Main.kt
@@ -1,4 +1,4 @@
-package com.vpaclov
+package com.vpavlov
 
 import com.fasterxml.jackson.core.JsonParseException
 import com.fasterxml.jackson.databind.ObjectMapper
